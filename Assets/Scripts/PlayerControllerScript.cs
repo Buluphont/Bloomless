f08ec2cf034d03cb2fd215bf345e01b81b18acf9
Phong Huynh <cps.phong@gmail.com>
2017-04-29 17:20:07 -0700

    Align movement vector to camera facing, fetch CharacterController object only onStart

--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -5,14 +5,21 @@ public class PlayerControllerScript : MonoBehaviour {
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
+	public Camera trackingCamera;
+
+	private CharacterController controller;
     private Vector3 moveDirection = Vector3.zero;
+
+	void Start(){
+		controller = GetComponent<CharacterController>();
+	}
+
     void Update()
-    {
-        CharacterController controller = GetComponent<CharacterController>();
+	{
         if (controller.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection = alignVectorTo(moveDirection, trackingCamera.transform);
             moveDirection *= speed;
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
@@ -21,4 +28,15 @@ public class PlayerControllerScript : MonoBehaviour {
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
     }
+
+	private Vector3 alignVectorTo(Vector3 vector, Transform target){
+		// Rotate along camera axes
+		vector = target.TransformDirection(vector);
+
+		// Negate y component, restore magnitude lost from negating y component
+		float originalMagnitude = vector.magnitude;
+		vector.y = 0;
+		vector = vector.normalized * originalMagnitude;
+		return vector;
+	}
 }
