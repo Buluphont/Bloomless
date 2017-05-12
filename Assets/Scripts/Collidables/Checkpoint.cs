e7e43c70ee4bab949e08304e26d4902d11afa4b3
Phong Huynh <cps.phong@gmail.com>
2017-05-12 09:57:59 -0700

    Missed the new files. whoops.

---
+++ Assets/Scripts/Collidables/Checkpoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+	private CheckpointController checkpointController;
+
+	void Start () {
+		this.checkpointController = GameObject.FindWithTag("Game State Manager").GetComponent<CheckpointController>();
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Player")) {
+			Debug.Log("New checkpoint set");
+			checkpointController.SetCheckpoint(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Collidables/Pickups/PollenPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PollenPickup : Pickup {
+	public enum PollenType {
+		Red,
+		Green,
+		Blue
+	}
+
+	public PollenType type;
+
+	protected override void onPickedUp(GameObject picker){
+		picker.GetComponent<Inventory>().AddPollen(this.type);
+		Destroy(this.gameObject);
+	}
+}
