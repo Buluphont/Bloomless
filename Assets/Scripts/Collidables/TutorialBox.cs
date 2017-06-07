ff6b418738f0beff686bd2ed2c3575719058b4a8
Phong Huynh <cps.phong@gmail.com>
2017-06-06 21:37:12 -0700

    Add tutorial text blasting script

---
+++ Assets/Scripts/Collidables/TutorialBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialBox : MonoBehaviour {
+	public GameObject tutorialText;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			tutorialText.SetActive(true);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			tutorialText.SetActive(false);
+		}
+	}
+}
