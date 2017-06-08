3665b197d2e60b2d34bc7ec65d394ddb6774398b
Zachary Cloutier <zcloutie@uci.edu>
2017-06-07 17:52:18 -0700

    Adding Enemy Scripts and Prefab

---
+++ Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+	public float patrolspeed = 2f;
+	public float chaseSpeed = 5f;
+	public float chaseWaitTime = 5f;
+	public float patrolWaitTime = 1f;
+	public Transform[] patrolWayPoints;
+
+	private EnemySight enemysight;
+	private UnityEngine.AI.NavMeshAgent nav;
+	private Transform player;
+	private float chasetimer;
+	private float patroltimer;
+	private int waypointIndex;
+
+	void Awake(){
+		enemysight = GetComponent<EnemySight> ();
+		nav = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void Update(){
+		if (enemysight.playerInSight)
+			Chasing ();
+		else
+			Patrolling ();
+	}
+
+	void Chasing(){
+		Vector3 sightingDeltaPos = enemysight.personalLastSighting - transform.position;
+		if (sightingDeltaPos.sqrMagnitude > 4f)
+			nav.destination = enemysight.personalLastSighting;
+
+		nav.speed = chaseSpeed;
+		if (nav.remainingDistance < nav.stoppingDistance) {
+			chasetimer += Time.deltaTime;
+			if (chasetimer > chaseWaitTime) {
+				chasetimer = 0f;
+			}
+		} else
+			chasetimer = 0f;
+	}
+
+	void Patrolling(){
+		nav.speed = patrolspeed;
+
+		if (nav.remainingDistance < nav.stoppingDistance) {
+			patroltimer += Time.deltaTime;
+
+			if (patroltimer >= patrolWaitTime) {
+				if (waypointIndex == patrolWayPoints.Length - 1)
+					waypointIndex = 0;
+				else
+					waypointIndex++;
+
+				patroltimer = 0f;
+			}
+		} 
+		else
+			patroltimer = 0f;
+
+		nav.destination = patrolWayPoints[waypointIndex].position;
+	}
+}
+	
---
+++ Assets/Scripts/EnemyScripts/EnemySight.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour {
+
+	public float fieldOfViewAngle = 110f;
+	public bool playerInSight;
+	public Vector3 personalLastSighting;
+
+	private UnityEngine.AI.NavMeshAgent nav;
+	private SphereCollider col;
+	private GameObject player;
+	private Vector3 previousLastSighting;
+
+	void Awake(){
+		nav = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		col = GetComponent<SphereCollider> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.gameObject == player) {
+			playerInSight = false;
+
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle (direction, transform.forward);
+
+			if (angle < fieldOfViewAngle * 0.5f) {
+				RaycastHit hit;
+
+				if (Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, col.radius)){
+					if(hit.collider.gameObject == player){
+						playerInSight = true;
+						previousLastSighting = personalLastSighting;
+						personalLastSighting = player.transform.position;
+					}
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject == player)
+			playerInSight = false;
+	}
+}
